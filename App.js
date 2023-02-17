c111f980b3432fb9b7afe0bc6dd760a0dec7a763
KenChui-701 <chuikin701@gmail.com>
2023-02-17 14:53:02 +0800

    v1

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { useState } from 'react';
 export default function App() {
+  let [data, setdata] = useState("Hello world")
+  let [data2, setdata2] = useState([
+    {name:"ken"},
+    {name:"ken"},
+    {name:"ken"},
+    {name:"ken"},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data}</Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="e.g. Ken"
+        onChangeText={(val)=>{
+          setdata(val)
+        }}
+      />
+      {data2.map((val) => {
+        return <Text> {val.name} </Text>
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +35,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth: 1,
+    borderColor:"#777",
+    width: 200
+  }
 });
